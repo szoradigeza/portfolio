65b253a7e7130f8e9e99957fd208e955ad203c5d
szoradigeza <szoradigeza@gmail.com>
2020-02-12 20:07:54 +0100

    Updates

---
+++ precache-manifest.58b943382dfcfa2cb3998c17bd2965e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3c9a38dbcc0bbe42affae59a07d5ab",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "989bc562bb24f0d6b0ef",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3e1e32f06439d0216cd5",
+    "url": "/portfolio/static/css/main.472ee3bf.chunk.css"
+  },
+  {
+    "revision": "989bc562bb24f0d6b0ef",
+    "url": "/portfolio/static/js/2.dfc9ae9d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.dfc9ae9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1e32f06439d0216cd5",
+    "url": "/portfolio/static/js/main.795a400f.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "af1feba1e94447112160c7d89c3a35d6",
+    "url": "/portfolio/static/media/Meticula.af1feba1.ttf"
+  },
+  {
+    "revision": "8990aa4273f67dba5cc15a34d46f6a03",
+    "url": "/portfolio/static/media/fotis.8990aa42.jpg"
+  }
+]);
